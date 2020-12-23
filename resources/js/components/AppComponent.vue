1c08f4d3930488462919068949cb69a87c47ced4
Rbruno <rodrigobruno74@gmail.com>
2020-12-23 12:16:47 +0100

    Update AppComponent.vue
    Agregado el footer

--- resources/js/components/AppComponent.vue
+++ resources/js/components/AppComponent.vue
@@ -1,37 +1,42 @@
 <template>
-     <v-app>
-      <v-col>
-        
-        <v-row class="justify-center">
-          <h1 class="blue--text text--darken-2">EUROPE LANGUAGE JOBS TEST </h1>
-        </v-row>  
-        
-        <v-divider></v-divider>
+  <v-app>
+    <v-col>
 
-        <v-row class="justify-center w-100">
-          <CargaLista-component  v-on:UpdateLista="UpdateLista" ></CargaLista-component>
-        </v-row> 
-        
-        <v-row class="justify-center w-100 mt-3">
-          <div class="flex-row d-inline w-50">
-            <Lista-component 
-              v-bind:listas="this.listas"
-              v-on:UpdateLista="UpdateLista" 
-              v-on:UpdateProducto="UpdateProducto"
-            >
-            </Lista-component>
-          </div>
-          <div class="flex-row d-inline w-50">
-            <ContenidoLista-component
-              v-bind:nombreLista="this.nombreLista" 
-              v-bind:idLista="this.idLista" 
-              v-bind:productos="this.productos" 
-              v-on:UpdateProducto="UpdateProducto"
-            ></ContenidoLista-component>
-          </div>
-        </v-row>    
-       
-      </v-col>
+      <v-row class="justify-center">
+        <h1 class="blue--text text--darken-2">EUROPE LANGUAGE JOBS TEST </h1>
+      </v-row>  
+      
+      <v-divider></v-divider>
+
+      <v-row class="justify-center w-100">
+        <CargaLista-component  v-on:UpdateLista="UpdateLista" ></CargaLista-component>
+      </v-row> 
+      
+      <v-row class="justify-center w-100 mt-3">
+        <div class="flex-row d-inline w-50">
+          <Lista-component 
+            v-bind:listas="this.listas"
+            v-on:UpdateLista="UpdateLista" 
+            v-on:UpdateProducto="UpdateProducto"
+          >
+          </Lista-component>
+        </div>
+        <div class="flex-row d-inline w-50">
+          <ContenidoLista-component
+            v-bind:nombreLista="this.nombreLista" 
+            v-bind:idLista="this.idLista" 
+            v-bind:productos="this.productos" 
+            v-on:UpdateProducto="UpdateProducto"
+          ></ContenidoLista-component>
+        </div>
+      </v-row>    
+      
+    </v-col>
+    <v-footer padless color="blue lighten-5">
+        <v-col class="text-center" cols="12" >
+         <strong>2020</strong> — <strong class="blue--text text--darken-2">Rodrigo Bruno</strong>
+        </v-col>
+    </v-footer>
   </v-app>
 </template>
 <script>
